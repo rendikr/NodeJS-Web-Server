9c2a5b67e13d7c3d1ba3006ba4e108338919efe7
Rendi K <rendi.krinaldi@gmail.com>
2020-09-18 05:14:40 +0700

    query string

--- src/app.js
+++ src/app.js
@@ -41,11 +41,30 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address'
+    })
+  }
+
   res.send({
     title: 'Weather',
+    address: req.query.address,
     location: 'Jakarta',
     temperature: '32',
     forecast: 'The weather is mild'
+  })
+})
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: 'You must provide a search term'
+    })
+  }
+
+  res.send({
+    products: []
   })
 })
 
