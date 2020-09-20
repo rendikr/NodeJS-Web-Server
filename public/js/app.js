c23bc4c254df404bd76f79db8438ef745434d16f
Rendi K <rendi.krinaldi@gmail.com>
2020-09-20 22:07:49 +0700

    search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,15 @@
 console.log('Client side javascript file is loaded')
 
-fetch('http://localhost:3000/weather?address=jakarta').then((response) => {
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
   response.json().then((data) => {
     if (data.error) {
       console.log(data.error)
@@ -9,4 +18,5 @@ fetch('http://localhost:3000/weather?address=jakarta').then((response) => {
       console.log(data.forecast)
     }
   })
+})
 })
